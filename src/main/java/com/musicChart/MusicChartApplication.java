d15beb9f65758385dd3893034265ad9d48f6f432
Natnaree Asavaseri <nasavas@ap.xom.com>
2020-04-17 22:18:34 +0700

    initial init

---
+++ src/main/java/com/musicChart/MusicChartApplication.java
@@ -0,0 +1,13 @@
+package com.musicChart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicChartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicChartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/musicChart/MusicChartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicChart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicChartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
