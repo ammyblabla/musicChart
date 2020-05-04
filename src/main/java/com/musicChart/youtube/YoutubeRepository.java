690c1b155dd32c7fc3c556637e2c6e715efcb4bc
Natnaree Asavaseri <nasavas@ap.xom.com>
2020-05-04 22:16:51 +0700

    delete unused code

--- src/main/java/com/musicChart/youtube/YoutubeRepository.java
+++ src/main/java/com/musicChart/youtube/YoutubeRepository.java
@@ -16,7 +16,6 @@ import java.security.GeneralSecurityException;
 public class YoutubeRepository {
     @Autowired
     private YoutubeConfiguration youtubeConfiguration;
-//    private YoutubeConfiguration youtubeConfiguration = new YoutubeConfiguration();
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
     public static YouTube getService() throws GeneralSecurityException, IOException {
@@ -28,7 +27,6 @@ public class YoutubeRepository {
 
     public VideoListResponse getVideoListResponse() throws IOException, GeneralSecurityException {
         YouTube.Videos.List youtubeRequest = getService().videos().list("snippet,localizations");
-        System.out.println("key " + youtubeConfiguration.getDeveloperKey());
         return youtubeRequest.setKey(youtubeConfiguration.getDeveloperKey())
                 .setChart("mostPopular")
                 .setRegionCode("TH")
